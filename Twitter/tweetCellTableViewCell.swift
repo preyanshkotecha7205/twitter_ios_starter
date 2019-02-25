a8902ada10ae762f6955f161af233b6ac235252c
preyanshkotecha7205 <preyanshkotecha@gmail.com>
2019-02-25 07:03:13 -0800

    Bonus Commit

---
+++ Twitter/tweetCellTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  tweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Preyansh Kotecha on 2/25/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class tweetCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
